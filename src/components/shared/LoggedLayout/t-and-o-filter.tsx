1ba35114ded461389ccc74922f34bd18a21b78a5
Abinash88 <subediabinas@gmail.com>
2024-10-31 14:17:13 +0545

    fix: header filter

--- src/components/shared/LoggedLayout/t-and-o-filter.tsx
+++ src/components/shared/LoggedLayout/t-and-o-filter.tsx
@@ -2,20 +2,33 @@ import { DatePicker } from "antd";
 import { ChevronDown } from "lucide-react";
 import CustomPopover from "../ReuseAble/custom-popover";
 import { DropDownBox } from "../custom-dropdown";
-import { dropDownData } from "@/lib/utils";
+import { cn, dropDownData } from "@/lib/utils";
 import { useState } from "react";
 
 const TAndOFilter = () => {
   const [country, setCountry] = useState("");
+  const [select, setSelect] = useState("");
   return (
     <div className="w-full p-4 border rounded-xl h-full">
       <div className="">
         <h3 className="font-medium md:text-base text-sm my-2">Risk Type</h3>
         <div className="flex gap-4 items-center justify-between">
-          <button className=" py-1.5 w-[50%] text-white bg-red-600 text-center rounded-lg ">
+          <button
+            onClick={() => setSelect("Threats")}
+            className={cn(
+              ` py-1.5 w-[50%] text-white bg-red-600/80 text-center rounded-lg `,
+              select === "Threats" && "bg-red-700"
+            )}
+          >
             Threats
           </button>
-          <button className=" py-1.5 w-[50%] text-white bg-blue-600 text-center rounded-lg ">
+          <button
+            onClick={() => setSelect("Opportunities")}
+            className={cn(
+              ` py-1.5 w-[50%] text-white bg-blue-600/80 text-center rounded-lg `,
+              select === "Opportunities" && "bg-blue-700"
+            )}
+          >
             Opportunities
           </button>
         </div>
@@ -24,15 +37,20 @@ const TAndOFilter = () => {
       <div className="">
         <h3 className="font-medium md:text-base text-sm my-2">Region</h3>
         <div className="flex gap-4 items-center justify-between">
-          <button className="  py-1.5 w-[50%] text-white bg-[#00C7BE] text-center rounded-lg ">
+          <button
+            onClick={() => {
+              setCountry("global");
+            }}
+            className="  py-1.5 w-[50%] text-white bg-[#00C7BE] text-center rounded-lg "
+          >
             Global
           </button>
           <div className="w-[50%]">
             <CustomPopover
               btnClassName="w-full "
               popIcon={
-                <button className=" flex   w-full gap-2 j border justify-center py-1.5  text-white bg-[#A2845E] text-center rounded-lg ">
-                  <span>Select Country</span> <ChevronDown />
+                <button className=" flex   w-full gap-2 font-normal border justify-center py-1.5  text-white bg-[#A2845E] text-center rounded-lg ">
+                  <span>{country || "Select Country"}</span> <ChevronDown />
                 </button>
               }
             >
